acbf873b9c6b4e0001112ae0e21a3b7b5ef3674b
RANGROO <reecemeakings@gmail.com>
2026-09-23 22:24:31 -0700

    Avoid password prompts in Higgs service controls

--- conf.sh
+++ conf.sh
@@ -4,10 +4,35 @@ cd -- "$(dirname -- "$(readlink -f -- "$0")")"
 echo 'Higgs TTS 3: 1 Enable startup, 2 Start, 3 Stop and free GPU memory, 4 Change model (stop Higgs first), 0 Disable'
 read -r -p 'Select: ' choice
 case "$choice" in
-    1) ./service.sh enable ;;
-    2) sudo ./service.sh start ;;
-    3) sudo ./service.sh stop ;;
-    4) bash ./install.sh ask ;;
-    0) sudo ./service.sh disable ;;
+    1)
+        if [ "$(id -u)" -eq 0 ]; then
+            runuser -u dwemer -- ./service.sh enable
+        else
+            ./service.sh enable
+        fi
+        ;;
+    2|3|0)
+        case "$choice" in
+            2) action=start ;;
+            3) action=stop ;;
+            0) action=disable ;;
+        esac
+        if [ "$(id -u)" -eq 0 ]; then
+            ./service.sh "$action"
+        else
+            # Older launchers must fail promptly rather than ask for a Linux password.
+            sudo -n ./service.sh "$action" || {
+                echo 'Unable to control Higgs. Use an updated DwemerDistro launcher and check the service error above.' >&2
+                exit 1
+            }
+        fi
+        ;;
+    4)
+        if [ "$(id -u)" -eq 0 ]; then
+            runuser -u dwemer -- bash ./install.sh ask
+        else
+            bash ./install.sh ask
+        fi
+        ;;
     *) echo 'No changes made.' ;;
 esac
